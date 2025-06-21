aadf8bc6b48371fdf7b89d1bc4a3fcdbbfb858f0
Ratna <sonawane.ratna.5057@gmail.com>
2025-06-21 10:05:26 +0530

    update

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -85,7 +85,7 @@ const Banner = () => {
                     </div>
                     <div className="slide-up-and-fade">
                         <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
-                            7K+
+                            7.3K+
                         </h5>
                         <p className="text-muted-foreground">Hours Worked</p>
                     </div>
